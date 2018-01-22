8bdeed4d9017c400f4b1f119fc343dcb717b249d
xiaoeric <alpha2275@gmail.com>
2018-01-21 23:24:07 -0800

    Editted for mini TT

--- auto.cpp
+++ auto.cpp
@@ -34,42 +34,55 @@ int main()
   int time_start = 0;
   int seconds = 0;
 
+  cout << endl;
+
   while(true){
     time_t now = time(0);
 
+    cout << "Initiating battle!" << endl;
     system("adb shell input tap 500 1800"); //Hard 4
     sleep(.5);
-    system("adb shell input tap 500 1300"); //Confirm fight
+    system("adb shell input tap 540 1450"); //Confirm fight
 
     if(stamina < staminaPerRun){
+      cout << "Only " << stamina << " stamina left! Refilling..." << endl;
       sleep(.5);
       system("adb shell input tap 500 1200"); //Confirm refill
       stamina += 99;
       sleep(1);
       system("adb shell input tap 500 1200"); //Close
+      cout << "Stamina refilled!" << endl;
     }
 
     stamina -= staminaPerRun;
 
     sleep(5);
 
+    cout << "Initiating auto-battle!" << endl;
     system("adb shell input tap 800 2000"); //Auto Battle
     sleep(.25);
     system("adb shell input tap 500 1100"); //Confirm auto battle
 
-    sleep(75);
+    cout << "Waiting for battle to finish..." << endl;
+    sleep(80);
 
     points += scorePerRun;
 
     system("adb shell input tap 500 1100"); //Stage clear
-    sleep(6);
+    cout << "Battle completed! Waiting for confirmation..." << endl;
 
+    sleep(8);
+
+    cout << "Confirmed!" << endl;
     system("adb shell input tap 500 1800"); //Ok
     sleep(1);
 
     if(points >= reward){
+      cout << "You've unlocked an award at " << reward << " points!" << endl;
       system("adb shell input tap 500 1400"); //Redeem
-      reward += 2500;
+      //reward += 2500; normal TT
+      reward += 1250; //mini TT
+      cout << "You're next award is available at " << reward << " points!" << endl;
       sleep(1);
     }
 
@@ -81,7 +94,12 @@ int main()
       seconds -= 300;
     }
 
+    cout << "--Postbellum Report--" << endl;
+    cout << "Stamina: " << stamina << endl;
+    cout << "Points: " << points << endl;
+
     if(points >= target){
+      cout << "You've reached your target!" << endl;
       break;
     }
     sleep(1);
